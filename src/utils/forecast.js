88fcdb1da9b945083cff945267b517573dbc0db8
unknown <A_Christoforidis@hotmail.com>
2021-06-26 13:30:57 +0300

    Add extra Data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (longitude,latitude, callback)=>{
 
-    const url = 'http://api.weatherstack.com/current?access_key=d393cf6750be7adc5d46ce2701a2d2f3&query=' + latitude+','+longitude
+    const url = 'http://api.weatherstack.com/current?access_key=d393cf6750be7adc5d46ce2701a2d2f3&query=' + longitude+','+latitude
     
     request({url, json:true},(error,{body})=>{
         if(error){
@@ -11,7 +11,8 @@ const forecast = (longitude,latitude, callback)=>{
             console.log('Unable to find location')
         }else {
             
-                callback(undefined,body.current.weather_descriptions[0] + ' It is currently '+ body.current.temperature+' And it feels like '+ body.current.feelslike + ' Degrees out')
+                callback(undefined,body.current.weather_descriptions[0] + ' It is currently '+ body.current.temperature+' And it feels like '+ body.current.feelslike + ' Degrees out and th humidity is '+body.current.humidity)
+                
         
         }
     })
